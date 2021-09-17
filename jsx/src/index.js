24256f8c6bab68cbcf19cdc2af9166f9951b12f4
ufo607 <ufo607.off@gmail.com>
2021-09-17 10:21:20 +0500

    Finish Section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,9 +2,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
+
 //Create a react component
 const App = () => {
-  return <div>Hello!</div>;
+  function getTime() {
+    return (new Date()).toLocaleTimeString()
+  }
+  
+  function refresh() {
+    window.location.reload();
+  }
+  
+  const ButtonText = {  text: 'Click me'};
+  const style={ backgroundColor: 'blue', color: 'white'}
+  const labelText = 'Hello Enter text'
+
+
+  return (
+    <div style={{ backgroundColor: 'red', color: 'white'}} className="div">
+      <label htmlFor="name">{labelText}</label>
+      <input id="name" type="text" />
+      <button  style={style} onClick={refresh}>{ButtonText.text}</button><br />
+      <h1>Time</h1><h2>{getTime()}</h2>
+    </div>
+  )
 };
 
 //Take the react component and show it on the screen
